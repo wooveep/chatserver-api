9c3e0ffbbdb2320eb3647ac637b5bf2e0bc5a8d9
cloudyi <lyt.ccie@outlook.com>
2023-06-01 13:01:29 +0800

    update TODO

---
+++ pkg/search/search.go
@@ -0,0 +1,10 @@
+/*
+ * @Author: cloudyi.li
+ * @Date: 2023-06-01 08:53:25
+ * @LastEditTime: 2023-06-01 08:53:41
+ * @LastEditors: cloudyi.li
+ * @FilePath: /chatserver-api/pkg/search/search.go
+ */
+package search
+
+// 对接Bing API。通过query查询关键词
